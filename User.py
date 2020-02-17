638bf2f1dcd5cc0583d77eda2a9007270da6885a
munbo <bxm18zqu@uea.ac.uk>
2020-02-17 17:26:28 +0000

    SQLAlchemy User.py

--- User.py
+++ User.py
@@ -1,56 +1,80 @@
 from enum import Enum
 import datetime
-from Group import Group
+
+import sqlalchemy
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,relationship
+
+#from Group import Group
 from GoalInterface import GoalInterface
 
+Base = declarative_base()
+engine = create_engine('sqlite:///User.db', echo=True)
 
-class User:
-    def __init__(self, username, name, email, dob, password, gender, age, weight, height):
-        #load account details
-        self.__username = username
-        self.__email = email
-        self.__password = password
-        # load user details
-        self.__name = name
-        self.__dob = datetime.datetime.strptime(dob, "%d/%m/%Y").date()
-        Gender = Enum('Gender', 'Male Female PREFER_NOT_TO_SAY')
-        self.__gender = Gender[gender.upper()]
-        self.__age = age
-        self.__weight = weight
-        self.__height = height
-        self.__goals = []
-        self.__groups = []
-        print('\n'.join("%s: %s" % item for item in vars(self).items()))
 
-    def create_group(self):
-        """
+class User(Base):
+    __tablename__ = "User"
 
-        """
-        # Create a new group
-        group_goal = None
-        group = Group('name', 'OPEN', [self], [self], group_goal)
-        self.__groups.append(group)
+    username = Column('username', String, primary_key=True)
+    email = Column('email', String, unique=True)
+    password = Column('password', String)
+    name = Column('name', String)
 
-    def join_group(self, id):
-        pass
+    #
+    # def __init__(self, username, name, email, dob, password, gender, age, weight, height):
+    #     #load account details
+    #     self.__username = username
+    #     self.__email = email
+    #     self.__password = password
+    #     # load user details
+    #     self.__name = name
+    #     self.__dob = datetime.datetime.strptime(dob, "%d/%m/%Y").date()
+    #     Gender = Enum('Gender', 'Male Female')
+    #     self.__gender = Gender[gender.upper()]
+    #     self.__age = age
+    #     self.__weight = weight
+    #     self.__height = height
+    #     self.__goals = []
+    #     self.__groups = []
+    #     print('\n'.join("%s: %s" % item for item in vars(self).items()))
+    #
+    # def create_group(self):
+    #     """
+    #
+    #     """
+    #     # Create a new group
+    #     group_goal = None
+    #     group = Group('name', 'OPEN', [self], [self], group_goal)
+    #     self.__groups.append(group)
+    #
+    # def join_group(self, id):
+    #     pass
+    #
+    # def leave_group(self):
+    #     pass
+    #
+    # def edit_details(self):
+    #     pass
+    #
+    # def create_goal(self):
+    #     pass
+    #
+    # def create_exercise(self):
+    #     pass
+    #
+    # def create_diet(self):
+    #     pass
 
-    def leave_group(self):
-        pass
 
-    def edit_details(self):
-        pass
-
-    def create_goal(self):
-        pass
-
-    def create_exercise(self):
-        pass
-
-    def create_diet(self):
-        pass
 
 def main():
-    user = User('user', 'tim', 't123@gmail.com', '04/3/1990', 'password', 'male', 30, 60, 180)
+    #Base.metadata.create_all(bind=engine)
+    Session=sessionmaker(bind=engine)
+    session=Session()
+    users=session.query(User).all()
+    for user in users:
+        print('\n'.join("%s: %s" % item for item in vars(user).items()))
 
 if __name__ == '__main__':
     main()
