5b74f0bdcf32a764bb67522fb46baf40ba7aca4b
munbo <bxm18zqu@uea.ac.uk>
2020-02-19 01:54:02 +0000

    SQLAlchemy User.py

--- User.py
+++ User.py
@@ -1,25 +1,44 @@
-from enum import Enum
-import datetime
+# To do:
+# 1.Optimise imports
+# 2.Reformat dob into dd/mm/yyyy
+# 3.Think about how to store group arrays
+# 4.Define all database constraints
 
-import sqlalchemy
-from sqlalchemy import create_engine, Column, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker,relationship
+import enum
+import re
+from datetime import datetime
 
-#from Group import Group
-from GoalInterface import GoalInterface
+from sqlalchemy import *
+from sqlalchemy import types, create_engine
+from sqlalchemy.dialects.sqlite import DATE
+from sqlalchemy.ext.declarative import *
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 engine = create_engine('sqlite:///User.db', echo=True)
 
 
+class Gender(enum.Enum):
+    male = 1
+    female = 2
+    other = 3
+
+
 class User(Base):
     __tablename__ = "User"
-
-    username = Column('username', String, primary_key=True)
+    # We do not need to define a constructor. See main() for example
+    username = Column('username', String, primary_key=True)  # these attributes map directly to columns in the table
     email = Column('email', String, unique=True)
     password = Column('password', String)
     name = Column('name', String)
+    dob = Column('dob', DATE(
+        storage_format="%(month)02d/%(day)02d/%(year)04d",
+        regexp=re.compile("(?P<month>\d+)/(?P<day>\d+)/(?P<year>\d+)")
+    ))  # impose strict formatting mm-dd-yyyy
+    age = Column('age', INTEGER)
+    weight = Column('weight', INTEGER)
+    height = Column('height', REAL)
+    gender = Column(types.Enum('male', 'female', 'other'), nullable=false)
 
     #
     # def __init__(self, username, name, email, dob, password, gender, age, weight, height):
@@ -67,14 +86,25 @@ class User(Base):
     #     pass
 
 
-
 def main():
-    #Base.metadata.create_all(bind=engine)
-    Session=sessionmaker(bind=engine)
-    session=Session()
-    users=session.query(User).all()
-    for user in users:
-        print('\n'.join("%s: %s" % item for item in vars(user).items()))
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    user = User()
+    user.dob = "12/20/1999"
+    user.password = "peen"
+    user.username = "micro peen"
+    user.email = "@gmail.com"
+    user.name = "peenocchio"
+    user.gender = 'male'
+    date_str = '09-19-2018'
+    date_object = datetime.strptime(date_str, '%m-%d-%Y').date()
+    user.dob=date_object
+
+    session.add(user)
+    session.commit()
+    session.close()
+
 
 if __name__ == '__main__':
     main()
