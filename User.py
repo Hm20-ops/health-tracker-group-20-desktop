576b9235a40cfca1d414277c6269de37e69381d7
munbo <bxm18zqu@uea.ac.uk>
2020-02-24 12:00:45 +0000

    Created ModelHandler

--- User.py
+++ User.py
@@ -5,7 +5,7 @@ TODO:
 '''
 from enum import Enum
 import re
-import Group
+#import Group
 from datetime import datetime,date
 
 from sqlalchemy import *
@@ -14,16 +14,10 @@ from sqlalchemy.dialects.sqlite import DATE
 from sqlalchemy.ext.declarative import *
 from sqlalchemy.orm import sessionmaker
 
+from ModelHandler import *
 
-
-Base = declarative_base()
-engine = create_engine('sqlite:///User.db', echo=True)
-
-
-# class Gender(Enum):
-#     male = 1
-#     female = 2
-#     other = 3
+# Base = declarative_base()
+# engine = create_engine('sqlite:///User.db', echo=True)
 
 
 class User(Base):
@@ -40,7 +34,28 @@ class User(Base):
     age = Column(INTEGER)
     weight = Column(REAL)         #both weight and height should be real
     height = Column(REAL)
-    gender = Column(Enum('male', 'female', 'other'), nullable=false)
+    gender = Column(Enum('male', 'female', 'other'), nullable=false)#
+
+    def create_user(self, username, email, password, name, dob, weight, height, gender):
+        # Session = sessionmaker(bind=engine)
+        # session = Session()  # we interact with db file in a session
+        session = make_session()
+        user = User()
+        user.password = password
+        user.username = username
+        user.email = email
+        user.name = name
+        user.gender = gender
+        date_object = datetime.strptime(dob, '%d/%m/%Y').date()  # converting a date string to date object. See format
+        user.dob = date_object
+        user.age = user.age_calculator()
+        user.weight = weight
+        user.height = height
+
+        session.add(user)
+        session.commit()  # need to commit for changes to appear in database
+
+        session.close()
 
     def create_group(self):
         # Create a new group
@@ -54,8 +69,22 @@ class User(Base):
     def leave_group(self):
         pass
 
-    def edit_details(self):
-        pass
+    def edit_details(self, username, email, password, name, dob, weight, height, gender):
+        Session = sessionmaker(bind=engine)
+        session = Session()  # we interact with db file in a session
+        self.password = password
+        self.username = username
+        self.email = email
+        self.name = name
+        self.gender = gender
+        date_object = datetime.strptime(dob, '%d-%m-%Y').date()  # converting a date string to date object. See format
+        self.dob = date_object
+        self.age = self.age_calculator()
+        self.weight = weight
+        self.height = height
+
+        session.commit()  # need to commit for changes to appear in database
+        session.close()
 
     def create_goal(self):
         pass
@@ -71,74 +100,34 @@ class User(Base):
         age = today.year - self.dob.year -((today.month, today.day) <(self.dob.month, self.dob.day))
         return age
 
-    #
-    # def __init__(self, username, name, email, dob, password, gender, age, weight, height):
-    #     #load account details
-    #     self.__username = username
-    #     self.__email = email
-    #     self.__password = password
-    #     # load user details
-    #     self.__name = name
-    #     self.__dob = datetime.datetime.strptime(dob, "%d/%m/%Y").date()
-    #     Gender = Enum('Gender', 'Male Female')
-    #     self.__gender = Gender[gender.upper()]
-    #     self.__age = age
-    #     self.__weight = weight
-    #     self.__height = height
-    #     self.__goals = []
-    #     self.__groups = []
-    #     print('\n'.join("%s: %s" % item for item in vars(self).items()))
-    #
-    # def create_group(self):
-    #     """
-    #
-    #     """
-    #     # Create a new group
-    #     group_goal = None
-    #     group = Group('name', 'OPEN', [self], [self], group_goal)
-    #     self.__groups.append(group)
-    #
-    # def join_group(self, id):
-    #     pass
-    #
-    # def leave_group(self):
-    #     pass
-    #
-    # def edit_details(self):
-    #     pass
-    #
-    # def create_goal(self):
-    #     pass
-    #
-    # def create_exercise(self):
-    #     pass
-    #
-    # def create_diet(self):
-    #     pass
-
+    def make_session(self):
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        return session
 
 def main():
-    Base.metadata.create_all(bind=engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()#we interact with db file in a session
-
+    #Base.metadata.create_all(bind=engine)
+    # Session = sessionmaker(bind=engine)
+    # session = Session()#we interact with db file in a session
+    #
     user = User()
-    user.password = "Hemal20"
-    user.username = "Munbodh20"
-    user.email = "munbodhhemal20@gmail.com"
-    user.name = "Hemal Munbodh"
-    user.gender = 'male'
-    date_str = '20-05-1999'
-    date_object = datetime.strptime(date_str, '%d-%m-%Y').date()#converting a date string to date object. See format
-    user.dob=date_object
-    user.age=user.age_calculator()#mitigates error of inputting wrong age
+    # user.password = "Hemal20"
+    # user.username = "Munbodh20"
+    # user.email = "munbodhhemal20@gmail.com"
+    # user.name = "Hemal Munbodh"
+    # user.gender = 'male'
+    # date_str = '20-05-1999'
+    # date_object = datetime.strptime(date_str, '%d-%m-%Y').date()#converting a date string to date object. See format
+    # user.dob=date_object
+    # user.age=user.age_calculator()#mitigates error of inputting wrong age
+    #
+    #
+    # session.add(user)
+    # session.commit()#need to commit for changes to appear in database
+    #
+    # session.close()
 
-
-    session.add(user)
-    session.commit()#need to commit for changes to appear in database
-
-    session.close()
-
+    user.create_user("Munbodh21", "munbodhhemal21@gmail.com", "Hemal21", "Hemal Munbodh1", '21/05/1999', "65", "175", "male")
 
 if __name__ == '__main__':
     main()
