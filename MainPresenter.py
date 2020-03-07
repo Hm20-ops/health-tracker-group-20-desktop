190a889a7b038035057f2d4f7b70381b6c4fed0f
whackteachers <33785383+whackteachers@users.noreply.github.com>
2020-03-07 16:10:11 +0000

    rename icon pictures update on presenter: now have logout, side bar still to be updated

--- MainPresenter.py
+++ MainPresenter.py
@@ -1,13 +1,18 @@
 import sys
 from functools import partial
+
+from PyQt5.QtCore import pyqtSlot
+
 import User
 from BasicGoal import BasicGoal
 from CustomGoal import *
 from Group import Group
 from Diet import *
+import RegisterPresenter
 from home_page import Ui_Health_tracker
 from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 
+nav_bar_map = ['HOME', 'PROFILE', 'GOAL', 'EXERCISE', 'DIET', 'GROUP', 'LOGOUT']
 
 class MainPresenter:
     def __init__(self, username):
@@ -26,14 +31,36 @@ class MainPresenter:
         self._view = MainWindow(_basic_goals)
         # self._view.create_account.clicked.connect(partial(self._view.stackedWidget.setCurrentIndex, 1))
         # self._view.cancel.clicked.connect(partial(self._view.stackedWidget.setCurrentIndex, 0))
+        self._view.side_nav_bar.itemClicked.connect(lambda :self.navigate())
+
         self._view.show()
+
+    @pyqtSlot(int)
+    def navigate(self):
+        row = nav_bar_map[self._view.side_nav_bar.currentIndex().row()]
+        return {'HOME': self.stub,
+                'PROFILE': self.stub,
+                'GOAL': self.stub,
+                'EXERCISE': self.stub,
+                'DIET': self.stub,
+                'GROUP': self.stub,
+                'LOGOUT': self.logout
+                }[row]()
+
+
+    def logout(self):
+        self._redirect_to = RegisterPresenter.RegisterPresenter()
+        self._view.close()
+        return self._redirect_to
 
     def init_goal(self):
         # session = self._basic_goal_model.make_session()
-        # basic_goals = session.query(BasicGoal).filter(BasicGoal.username=self._user)
+        # basic_goals = session.query(BasicGoal).filter(BasicGoal.username=self._user, BasicGoal.isMet is False)
         basic_goals = {{'username': self._user, 'target_weight': 50, 'date': date.today(), 'isMet': False}}
         self._view.display_all_goal(basic_goals)
 
+    def stub(self):
+        pass
 
 class MainWindow(QMainWindow, Ui_Health_tracker):
     def __init__(self, goal_list, *args, obj=None, **kwargs):
