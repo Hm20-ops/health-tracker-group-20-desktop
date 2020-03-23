fe1750099ebff17464e9d28b6fefa2e2357e6149
munbo <bxm18zqu@uea.ac.uk>
2020-03-23 19:41:07 +0000

    Merge branch 'jason' of https://bitbucket.org/flanky/health-tracker-group-20 into Hemal
     Conflicts:
    	BasicGoalProgress.py
    	CustomGoalProgress.py
    	Group.py
    	Model.db

--- RegisterPresenter.py
+++ RegisterPresenter.py
@@ -1,34 +1,51 @@
 import sys
-
-from PyQt5.QtTest import QTest
-from validate_email import validate_email
-import User
-import MainPresenter
-from User import User
 from functools import partial
-from signinView import Ui_signinWindow
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtCore import pyqtSlot, Qt
+from validate_email import validate_email
+
+import MainPresenter
+import User
+from User import User
 from helper import *
+from signinView import Ui_signinWindow
 
 '''
 RegisterPresenter controls the login and registration process of the signin view
 '''
+
+
 class RegisterPresenter:
-    def __init__(self):
+    loginPassed = False
+
+    def __init__(self, **kwargs):
         self._redirect_to = None
         self._view = MainWindow()
         self._model = User()
+
         self._view.login.clicked.connect(lambda: self.login())
         self._view.create_account.clicked.connect(partial(self._view.stackedWidget.setCurrentIndex, 1))
         self._view.cancel.clicked.connect(partial(self._view.stackedWidget.setCurrentIndex, 0))
         self._view.signup.clicked.connect(lambda: self.add_user())
+
+        if len(kwargs) == 2:
+            username = kwargs['username']
+            password = kwargs['password']
+            self.test_loginInput(username, password)
+
+        # self._view.close()
+        # if(self.loginPassed==False):
+        #     self._view.show()
+        # QTest.qWait(50000000)
         self._view.show()
 
     '''
     Add a new user to database when new user register a new account on the view
     that takes all the input from the registration form to the database
     '''
+
     def add_user(self):
         # inner function for data validation when user submit the data
         def _add_validation():
@@ -81,6 +98,7 @@ class RegisterPresenter:
     '''
     function to login to the home screen of the program with the input username and password on the view
     '''
+
     def login(self):
         # establish connection to the database
         session = self._model.make_session()
@@ -89,7 +107,7 @@ class RegisterPresenter:
         password = self._view.login_password.text()
         # check if username and password are correct
         check = session.query(User).filter(User.username == username, User.password == password).first() \
-                if len(username) != 0 or len(password) != 0 else False
+            if len(username) != 0 or len(password) != 0 else False
         session.close()
         # if the check is successful redirect to the user home page, otherwise display error message
         if check:
@@ -102,17 +120,20 @@ class RegisterPresenter:
             display_message("Login Error",
                             "username or password is incorrect")
 
-    def test_input(self, username, password):
-        self._view.login_username.setText(username)
-        self._view.login_password.setText(password)
+    def test_loginInput(self, registerPresenterWindow, username, password):
+        registerPresenterWindow._view.login_username.setText(username)
+        registerPresenterWindow._view.login_password.setText(password)
         QTest.mouseClick(self._view.login, Qt.LeftButton)
         return self._redirect_to
+        # self._redirect_to=MainPresenter.MainPresenter(username)
+        # self.loginPassed = isinstance(passed, MainPresenter.MainPresenter)
 
 
 class MainWindow(QMainWindow, Ui_signinWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- unit_test.py
+++ unit_test.py
@@ -1,39 +1,113 @@
 import sys
-import unittest
+from datetime import datetime
 
+import pytest
+from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import QApplication
 
-from RegisterPresenter import RegisterPresenter
+import ModelHandler
 from MainPresenter import MainPresenter
-from PyQt5.QtTest import QTest
+from RegisterPresenter import RegisterPresenter
+from User import User
+
+'''
+This unit test carries out 4 tests
+1. Insertion on database
+2. Login with correct username and incorrect password
+3. Login with correct username and blank password
+4. Login with blank username and blank password
+
+When a message box appears saying login failed, click 'OK' to perform next test.
+Otherwise, the application gets stuck in the event loop
+'''
 
 
-class MyTestCase(unittest.TestCase):
-	app = QApplication(sys.argv)
-	def test_success(self):
-		x = RegisterPresenter()
-		result = x.test_input('Munbodh21', 'Hemal21')
-		expected = MainPresenter('Munbodh21')
-		self.assertEqual(result.current_user, expected.current_user)
-		#app.exec_()
+@pytest.fixture(scope='module')
+def session():
+    session = ModelHandler.make_session()  # set up of session
+    yield session  # return session
+    session.rollback()  # teardown session. Rollback any changes to db
+    session.close()  # close session
 
-	def test_fail(self):
-		x = RegisterPresenter()
-		result = x.test_input('Munbodh21', 'alddjd')
-		expected = None
-		self.assertEqual(result, expected)
 
-	def test_blank_password(self):
-		x = RegisterPresenter()
-		result = x.test_input('NodeJS', '')
-		expected = None
-		self.assertEqual(result, expected)
+@pytest.fixture(scope='module')
+def dbLength(session):
+    rows = session.query(User).count()  # counts number of rows in database before add any data
+    print('db length is', rows)
+    yield rows  # return number of rows
 
-	def test_blank(self):
-		x = RegisterPresenter()
-		result = x.test_input('', '')
-		expected = None
-		self.assertEqual(result, expected)
 
-if __name__ == '__main__':
-	unittest.main()
+@pytest.fixture(scope='module')
+def add_user(session):
+    user = User()
+    user.password = 'password'
+    user.username = 'Covid19'
+    user.email = 'Covid19@gmail.com'
+    user.name = 'Coronavirus'
+    user.gender = 'other'
+    date_object = datetime.strptime('20/11/2019',
+                                    '%d/%m/%Y').date()  # converting a date string to date object. See format
+    user.dob = date_object
+    user.age = user.age_calculator()
+    user.weight = 89
+    user.height = 190
+    session.add(user)  # add this temporary user
+    yield session
+
+
+def test_addedUser(dbLength, add_user):
+    rowAfterAdd = add_user.query(User).count()  # count rows after addition of a user
+    assert rowAfterAdd == dbLength + 1  # expected is originadbLength+1
+
+
+def test_invalidLogin():
+    app = QApplication(sys.argv)
+    registerPresenterWindow = RegisterPresenter()
+    result = registerPresenterWindow.test_loginInput(registerPresenterWindow, 'Munbodh21',
+                                                     'alddjd')  # valid username but invalid password
+    QTest.qWait(2.0 * 1000)  # wait 2 seconds before closing login window
+    app.exit()
+    # registerPresenterWindow._view.close()
+    expected = None
+    assert result == expected
+
+
+def test_blank_password():
+    app = QApplication(sys.argv)
+    registerPresenterWindow = RegisterPresenter()
+    result = registerPresenterWindow.test_loginInput(registerPresenterWindow, 'NodeJS', '')  # blank password
+    QTest.qWait(2.0 * 1000)
+    app.exit()
+    # registerPresenterWindow._view.close()
+    expected = None
+    assert result == expected
+
+
+def test_blank():
+    app = QApplication(sys.argv)
+    registerPresenterWindow = RegisterPresenter()
+    result = registerPresenterWindow.test_loginInput(registerPresenterWindow, '', '')  # blank username
+    QTest.qWait(2.0 * 1000)
+    app.exit()
+    # registerPresenterWindow._view.close()
+    expected = None
+    assert result == expected
+
+
+def test_validLogin():
+    app = QApplication(sys.argv)
+    registerPresenterWindow = RegisterPresenter()
+    result = registerPresenterWindow.test_loginInput(registerPresenterWindow, 'Munbodh21',
+                                                     'Hemal21')  # valid username valid password
+    QTest.qWait(2.0 * 1000)  # wait 2 seconds before closing login window
+    app.exit()
+    expected = MainPresenter('Munbodh21')
+    assert result.current_user == expected.current_user
+
+
+def test_loginWithNoUserName():
+    app = QApplication(sys.argv)
+    registerPresenterWindow = RegisterPresenter()
+    result = registerPresenterWindow.test_loginInput(registerPresenterWindow, '', 'H')
+    QTest.qWait(4.0 * 1000)  # wait 2 seconds before closing login window
+    assert result == None
